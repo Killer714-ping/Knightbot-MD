fee927f68072779ea37ba93bd016910be27c6f38
Killer714-ping <nationgame354@gmail.com>
2025-07-21 15:48:18 +0100

    Update settings.js
    Keep going......., 😈😈😈

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "Jarvis",
+  botOwner: 'hottest', // Your name
   ownerNumber: '9152300835', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.0.6",
+  version: "2.0.7",
 };
 
 module.exports = settings;
